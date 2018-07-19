4a50283bd4a1be81b03b9ff1a09e6dea149948d7
feiwenli <lfw_dar@163.com>
2018-07-19 12:03:35 +0800

    3

--- Exam3/src/main/java/com/hand/App.java
+++ Exam3/src/main/java/com/hand/App.java
@@ -8,20 +8,61 @@ import java.util.*;
  * [10,11,12],2=>[21,23,24]}。排序不能使用List.sort() 方法。必须自己写排序方法。
  *
  */
+interface Cre{
+    void create();
+}
 
+class AMap implements Cre{
+
+    @Override
+    public void create(){
+        System.out.println("Hi~");
+    }
+
+}
+
+class AList implements Cre{
+    @Override
+    public void create(){
+        System.out.println("Hi~");
+    }
+}
+
+
+class CreFactory{
+
+    public Cre getCre(String CreType){
+        if(CreType == null){
+            return null;
+        }
+        if(CreType.equalsIgnoreCase("AMAP")){
+            return new AMap();
+        } else if(CreType.equalsIgnoreCase("ALIST")){
+            return new AList();
+        }
+        return null;
+    }
+}
 
 public class App 
 {
     public static void main( String[] args )
     {
-        Set<Integer> set = new HashSet<>();
-        List<Integer> list = new ArrayList<>();
-        for(int i=0;i<50;i++) {
-            Integer value = (int) (Math.random() * 100);
-            set.add(value/10);
-            list.add(value);
-        }
+//        Set<Integer> set = new HashSet<>();
+//        List<Integer> list = new ArrayList<>();
+//        for(int i=0;i<50;i++) {
+//            Integer value = (int) (Math.random() * 100);
+//            set.add(value/10);
+//            list.add(value);
+//        }
 
-        Map map = new HashMap<>();
+        CreFactory cc = new CreFactory();
+
+        Cre l1 = cc.getCre("AList");
+        l1.create();
+
+        Cre m1 = cc.getCre("AMap");
+        m1.create();
+
     }
 }
